9a0f48ff9a22e56d67aa070dab9e02b4022e7eb4
jeremyT69 <jeremy.thomas69@outlook.fr>
2023-02-26 18:46:08 +0100

    découpage des pages

--- index.php
+++ index.php
@@ -1,39 +1,21 @@
-<!DOCTYPE html>
-<html lang="fr">
+<?php
+  define ('_RECIPES_IMG_PATH_', 'uploads/recipes/');
+?>
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>CuisineA</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="assets/css/override-bootstrap.css">
-  <link rel="stylesheet" href="assets/css/style.css">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 
-</head>
+<?php
 
-<body>
+$recipes = [
+  ['title' => 'Mousse au Chocolat', 'Description' => 'Mousse au chocolat example text to build on the card title and make up the bulk of the card\'s content.', 'image' => '1-chocolate-au-mousse.jpg'],
+  ['title' => 'Gratin Dauphinois', 'Description' => 'Gratin dauphinois example text to build on the card title and make up the bulk of the card\'s content.', 'image' => '2-gratin-dauphinois.jpg'],
+  ['title' => 'Salade de Chèvre', 'Description' => 'Salade de chèvre example text to build on the card title and make up the bulk of the card\'s content.', 'image' => '3-salade.jpg'],
+];
 
-  <div class="container">
-    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-      <a href="index.php" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
-        <img src="/assets/images/logo-cuisinea-horizontal.jpg" alt="Logo CuisineA" width="250">
-      </a>
+require_once ('templates/header.php');
 
-      <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-        <li><a href="#" class="nav-link px-2 link-secondary">Home</a></li>
-        <li><a href="#" class="nav-link px-2 link-dark">Features</a></li>
-        <li><a href="#" class="nav-link px-2 link-dark">Pricing</a></li>
-        <li><a href="#" class="nav-link px-2 link-dark">FAQs</a></li>
-        <li><a href="#" class="nav-link px-2 link-dark">About</a></li>
-      </ul>
+?>
 
-      <div class="col-md-3 text-end">
-        <button type="button" class="btn btn-outline-primary me-2">Login</button>
-        <button type="button" class="btn btn-primary">Sign-up</button>
-      </div>
-    </header>
+
 
 
     <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
@@ -45,97 +27,31 @@
         <p class="lead">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque maxime aut cupiditate animi accusamus quaerat perferendis at culpa velit sunt quos a earum, voluptatibus ipsa obcaecati placeat error non asperiores?
           Sunt necessitatibus eveniet numquam. Dicta quibusdam, eos eaque similique possimus iure ullam delectus ea officiis ab, modi magni, nobis fuga tempora dignissimos suscipit soluta dolorum cumque illum velit sunt inventore.</p>
         <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-          <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Voir mes recettes</button>
+          <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Voir nos recettes</button>
         </div>
       </div>
     </div>
 
     <div class="row">
 
-      <div class="col-md-4">
-        <div class="card">
-          <img src="uploads/recipes/1-chocolate-au-mousse.jpg" class="card-img-top" alt="Mousse au chocolat">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-        </div>
-      </div>
+      <?php foreach ($recipes as $recipe) { ?>
 
-      <div class="col-md-4">
-        <div class="card">
-          <img src="uploads/recipes/2-gratin-dauphinois.jpg" class="card-img-top" alt="Mousse au chocolat">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+        <div class="col-md-4">
+          <div class="card">
+            <img src="<?=_RECIPES_IMG_PATH_.$recipe ['image']; ?>" class="card-img-top" alt="<?=$recipe ['title']; ?>">
+            <div class="card-body">
+              <h5 class="card-title"><?=$recipe ['title']; ?></h5>
+              <p class="card-text"><?=$recipe ['Description']; ?>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div class="col-md-4">
-        <div class="card">
-          <img src="uploads/recipes/3-salade.jpg" class="card-img-top" alt="Mousse au chocolat">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-        </div>
-      </div>
-      
+      <?php } ?>
+
     </div>
 
-
-    <footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top">
-      <div class="col mb-3">
-        <a href="/" class="d-flex align-items-center mb-3 link-dark text-decoration-none">
-          <svg class="bi me-2" width="40" height="32">
-            <use xlink:href="#bootstrap"></use>
-          </svg>
-        </a>
-        <p class="text-muted">© 2022</p>
-      </div>
-
-      <div class="col mb-3">
-
-      </div>
-
-      <div class="col mb-3">
-        <h5>Section</h5>
-        <ul class="nav flex-column">
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
-        </ul>
-      </div>
-
-      <div class="col mb-3">
-        <h5>Section</h5>
-        <ul class="nav flex-column">
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
-        </ul>
-      </div>
-
-      <div class="col mb-3">
-        <h5>Section</h5>
-        <ul class="nav flex-column">
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
-        </ul>
-      </div>
-    </footer>
-
-</body>
-
-</html>
+<?php
+require_once ('templates/footer.php');
+?>
+    
